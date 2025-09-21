5cec0230486fd60d0dc3f8f6af601d6506ddfac5
ABD11AEOFF <abdullaevsultan668@gmail.com>
2025-09-21 19:59:23 +0500

    tema ozgeriwi

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./app.css";
+
+
+function App() {
+  const [isBlack, setIsBlack] = useState(true);
+
+  const toggleTheme = () => {
+    setIsBlack(!isBlack); 
+  };
+
+  return (
+    <div
+      style={{
+        backgroundColor: isBlack ? "white" : "black", 
+        color: isBlack ? "white" : "black",           
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button
+        onClick={toggleTheme}
+        style={{
+          backgroundColor: isBlack ? "black" : "white", 
+          color: isBlack ? "white" : "black",
+          border: "2px solid gray",
+          padding: "10px 20px",
+          borderRadius: "8px",
+          cursor: "pointer",
+        }}
+      >
+        Переключить тему (Сейчас: {isBlack ? "black" : "white"})
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
